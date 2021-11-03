d127b3c5def3e3634bdafcc40343c1857be1d4f9
Kurpenov Kuat <kurpenov2003@gmail.com>
2021-11-03 09:21:36 +0600

    Reversed the brackets in 2_1.cpp

--- 2/Code/2_1.cpp
+++ 2/Code/2_1.cpp
@@ -5,23 +5,21 @@ double f(double x) {
     double b = 1.3;
 
     if (x < 1.3) {
-        return (log(b * x - 1)) - (1 / (b * x + 1));
+        return log(b * x) - (1 / (b * x + 1));
     } else if ((x >= 1.3) && (x <= 1.7)) {
         return b * x + 1;
     } else if (x > 1.7) {
-        return (log(b * x + 1)) - (1 / (b * x + 1));
+        return log(b * x) + (1 / (b * x + 1));
     }
 
     return 0;
 }
 
 int main() {
-    double x = 0;
+    double x;
 
-    while ((x < 1) || (x > 2)) {
-        std::cout << "[>] Enter X [1, 2]: ";
-        std::cin >> x;
-    }
+    std::cout << "[>] Enter X [1, 2]: ";
+    std::cin >> x;
 
     double result = f(x);
     std::cout << "[+] Result: " << result << std::endl;
