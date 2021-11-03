aa2fd0076a45cba644a127119004babc723e2cfb
Kurpenov Kuat <kurpenov2003@gmail.com>
2021-11-03 07:50:41 +0600

    Replaced the code from main with the function in 1_1.cpp

--- 1/Code/1_1.cpp
+++ 1/Code/1_1.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <cmath>
 
-int main() {
-    double a, b, x1, x2, y1, y2;
+double f(double a, double b, double x) {
+    double y = sqrt(std::abs(a * x * pow(sin(x), 2))) + (pow(M_E, -2 * x) * (x + b));
+    return y;
+}
 
-    x1 = 1;
-    x2 = 2;
+int main() {
+    double a, b, x1, x2;
+
+    x1 = 1.0;
+    x2 = 2.0;
     a = 0.5;
     b = 3.1;
 
-    y1 = sqrt(std::abs(a * x1 * pow(sin(x1), 2))) + (pow(M_E, -2 * x1) * (x1 + b));
-    std::cout << "[+] Y1: " << y1 << std::endl;
+    double y1 = f(a, b, x1);
+    std::cout << "[+] F(" << a << ", " << b << ", " << x1 << "): " << y1 << std::endl;
 
-    y2 = sqrt(std::abs(a * x2 * pow(sin(x2), 2))) + (pow(M_E, -2 * x1) * (x2 + b));
-    std::cout << "[+] Y2: " << y2 << std::endl;
+    double y2 = f(a, b, x2);
+    std::cout << "[+] F(" << a << ", " << b << ", " << x2 << "): " << y2 << std::endl;
 
     return 0;
 }
