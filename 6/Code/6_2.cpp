09edadabb7f0545b5f85fc3d0b4c04a3dd9cd4d8
Kurpenov Kuat <kurpenov2003@gmail.com>
2021-11-04 12:06:04 +0600

    Changed the natural logarithm to decimal, changed code style and changed count of iterations

--- 6/Code/6_2.cpp
+++ 6/Code/6_2.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 double f_1(double x) {
-    double result = pow(M_E, -x) * log(sqrt(x + 1));
+    double result = pow(M_E, -x) * log10(sqrt(x + 1));
     return result;
 }
 
@@ -10,6 +10,7 @@ double f_2(double x) {
     double result = x + sin(x);
     return result;
 }
+
 int main() {
     double start = 1;
     double stop = 2;
@@ -17,21 +18,18 @@ int main() {
     double value = 1;
     double step = (stop - start) / steps;
 
-    double f_1_results[12];
-    double f_2_results[12];
+    double f_1_results[(int)steps];
+    double f_2_results[(int)steps];
     std::cout << "+------------+------------+------------+" << std::endl;
     std::cout << "|    Value   |    F(1)    |    F(2)    |" << std::endl;
     std::cout << "|------------+------------+------------|" << std::endl;
-    for (int i = 0; i < steps; i++) {
-        value += step;
+    for (int i = 0; i <= steps; i++) {
         f_1_results[i] = f_1(value);
         f_2_results[i] = f_2(value);
-
         printf("|    %.2f    |    %.2f    |    %.2f    |\n", value, f_1_results[i], f_2_results[i]);
+        value += step;
     }
     std::cout << "+------------+------------+------------+" << std::endl;
 
     return 0;
 }
-
-
