fc95e6235926d5b07cb426d84b47c29ae53b2b1d
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-02-06 22:14:55 +0600

    Lite fixed log information

--- 2 semester/AP/Practics/2022-02-04/2.py
+++ 2 semester/AP/Practics/2022-02-04/2.py
@@ -9,6 +9,5 @@ z = 0.252 * 10**3
 g = (pow(y, x + 1) / (pow(abs(y - 2), 1/3) + 3)) +\
         ((x + y / 2) / (2 * abs(x + y))) * (pow(x + 1, -1 / sin(z)))
 
-print(x, y, z)
 print(f"[+] G: {g:2.4f}")
 
