db3a9b3ecea7b23ac277001a146d5ec942f7e933
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-02-13 14:33:39 +0600

    Hot fixed bug into some program

--- 2 semester/AP/Practics/2022-02-09/2_4.py
+++ 2 semester/AP/Practics/2022-02-09/2_4.py
@@ -1,10 +1,14 @@
 n = int(input("[>] Enter number: "))
 
 start = 0
-while start < 3:
-    n += n + 1
-    n *= n + 1
-    n += n + 1
+temp = n
+
+while start < 4:
+    temp += 1
+    n += temp
+    
+    temp += 1
+    n *= temp
 
     start += 1
 
