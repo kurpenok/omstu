e55609d5ac0a08208b079c318a4912c1be4f906e
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-02-21 23:02:13 +0600

    Added based functions to Dijkstra algorithm

--- 2 semester/DM/Practics/2022-02-17/Dijkstra.py
+++ 2 semester/DM/Practics/2022-02-17/Dijkstra.py
@@ -1,65 +1,55 @@
-def find_lowest_cost_node(costs: dict, processed: list) -> str:
-    lowest_cost = float("inf")
-    lowest_cost_node = None
-    for node in costs:
-        if costs[node] < lowest_cost and node not in processed:
-            lowest_cost = costs[node]
-            lowest_cost_node = node
-    return lowest_cost_node
+from queue import PriorityQueue
 
 
-graph = {}
+class Graph:
+    def __init__(self, numvП_of_vertices):
+        self.v = num_of_vertices
+        self.edges = [[-1 for i in range(num_of_vertices)] for j in range(num_of_vertices)]
+        self.visited = []
+    def add_edge(self, u, v, weight):
+        self.edges[u][v] = weight
+        self.edges[v][u] = weight
 
-graph["start"] = {}
-graph["start"]["a"] = 53
-graph["start"]["b"] = 87
-graph["start"]["c"] = 15
 
-graph["a"] = {}
-graph["a"]["b"] = 15
-graph["a"]["c"] = 44
-graph["a"]["d"] = 22
-graph["a"]["fin"] = 72
+def dijkstra(graph, start_vertex):
+    D = {v:float('inf') for v in range(graph.v)}
+    D[start_vertex] = 0
 
-graph["b"] = {}
-graph["b"]["c"] = 31
-graph["b"]["d"] = 17
-graph["b"]["fin"] = 14
+    pq = PriorityQueue()
+    pq.put((0, start_vertex))
 
-graph["c"] = {}
-graph["c"]["fin"] = 84
+    while not pq.empty():
+        (dist, current_vertex) = pq.get()
+        graph.visited.append(current_vertex)
 
-graph["d"] = {}
-graph["d"]["c"] = 16
+        for neighbor in range(graph.v):
+            if graph.edges[current_vertex][neighbor] != -1:
+                distance = graph.edges[current_vertex][neighbor]
+                if neighbor not in graph.visited:
+                    old_cost = D[neighbor]
+                    new_cost = D[current_vertex] + distance
+                    if new_cost < old_cost:
+                        pq.put((new_cost, neighbor))
+                        D[neighbor] = new_cost
+    return D
 
-graph["fin"] = {}
 
-costs = {}
-costs["a"] = 53
-costs["b"] = 87
-costs["c"] = 15
-costs["fin"] = float("inf")
+g = Graph(9)
+g.add_edge(0, 1, 4)
+g.add_edge(0, 6, 7)
+g.add_edge(1, 6, 11)
+g.add_edge(1, 7, 20)
+g.add_edge(1, 2, 9)
+g.add_edge(2, 3, 6)
+g.add_edge(2, 4, 2)
+g.add_edge(3, 4, 10)
+g.add_edge(3, 5, 5)
+g.add_edge(4, 5, 15)
+g.add_edge(4, 7, 1)
+g.add_edge(4, 8, 5)
+g.add_edge(5, 8, 12)
+g.add_edge(6, 7, 1)
+g.add_edge(7, 8, 3)
 
-parents = {}
-parents["a"] = "start"
-parents["b"] = "start"
-parents["c"] = "start"
-parents["fin"] = None
-
-processed = []
-
-node = find_lowest_cost_node(costs, processed)
-
-while node is not None:
-    cost = costs[node]
-    neightbors = graph[node]
-    for n in neightbors.keys():
-        new_cost = cost + neightbors[n]
-        if costs[n] > new_cost:
-            costs[n] = new_cost
-            parents[n] = node
-    processed.append(node)
-    node = find_lowest_cost_node(costs, processed)
-
-print(costs["fin"])
+print(dijkstra(g, 0))
 
