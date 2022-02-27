0710fd3cb49adc50b80bf2c9186f3f063cd2e75c
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-02-27 10:29:01 +0600

    Modified 9 Coursera solution

--- 2 semester/PW/Coursera/Python/3 Week/9.py
+++ 2 semester/PW/Coursera/Python/3 Week/9.py
@@ -1,17 +1,15 @@
 a = float(input())
 b = float(input())
 c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
 
-d = b**2 - 4 * a * c
+x = (b * f - d * e) / (b * c - a * d)
 
-if a == 0 and b == 0 and c == 0:
-    print(3)
-elif d < 0:
-    print(0)
-elif d == 0:
-    x = (-b + d**0.5) / (2 * a)
-    print(1, x)
-elif d > 0:
-    x1 = (-b + d**0.5) / (2 * a)
-    x2 = (-b - d**0.5) / (2 * a)
-    print(2, min(x1, x2), max(x1, x2))
+if b:
+    y = (e - a * x) / b
+else:
+    y = (f - c * x) / d
+
+print(x, y)
