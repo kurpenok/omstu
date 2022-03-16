61c5f2482f36ed74e88c9596126c241fc3bfc7f2
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-03-16 11:00:46 +0600

    Added Ford-Bellman algorithm

--- 2 semester/DM/Practics/2022-03-09/FordBellman.py
+++ 2 semester/DM/Practics/2022-03-09/FordBellman.py
@@ -1,72 +1,44 @@
-from collections import defaultdict
-
-
 class Graph:
-    def __init__(self) -> None:
-        self.vertexes = set()
-        self.weights = dict()
-        self.edges = defaultdict(list)
+    def __init__(self, vertexes: int) -> None:
+        self.vertexes = vertexes
+        self.graph = []
 
-    def addVertex(self, vertex: str) -> None:
-        self.vertexes.add(vertex)
+    def addEdge(self, u: int, v: int, w: int) -> None:
+        self.graph.append([u, v, w])
 
-    def addEdge(self, u: str, v: str, weight: int) -> None:
-        self.edges[u].append(v)
-        self.weights[(u, v)] = weight
+    def print(self, dist: list) -> None:
+        print("[+] Vertex distance from sourses")
+        for i in range(self.vertexes):
+            print(f"{i}\t\t{dist[i]}")
 
-    def dijkstra(self, start: str) -> None:
-        visited = {start: 0}
-        path = defaultdict(list)
+    def fordbellman(self, start: int):
+        dist = [float("inf")] * self.vertexes
+        dist[start] = 0
 
-        vertexes = self.vertexes
+        for _ in range(self.vertexes - 1):
+            for u, v, w in self.graph:
+                if dist[u] != float("inf") and dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
 
-        while vertexes:
-            minimal = None
-            for vertex in vertexes:
-                if vertex in visited:
-                    if minimal is None:
-                        minimal = vertex
-                    elif visited[vertex] < visited[minimal]:
-                        minimal = vertex
-            if minimal is None:
-                break
+        for u, v, w in self.graph:
+            if dist[u] != float("inf") and dist[u] + w < dist[v]:
+                print("[-] The graph has cycles")
+                return
 
-            vertexes.remove(minimal)
-            current_weight = visited[minimal]
-
-            for edge in self.edges[minimal]:
-                weight = current_weight + self.weights[(minimal, edge)]
-                if edge not in visited or weight < visited[edge]:
-                    visited[edge] = weight
-                    path[edge].append(minimal)
-
-        print("[+] Dijkstra result (visited vertexes): ", visited)
-        print("[+] Dijkstra result (full path): ", path)
-
-    def fordbellman(self, start: str) -> None:
-        pass
+        self.print(dist)
 
 
 if __name__ == "__main__":
-    graph = Graph()
+    graph = Graph(5)
 
-    graph.addVertex("a")
-    graph.addVertex("b")
-    graph.addVertex("c")
-    graph.addVertex("d")
-    graph.addVertex("e")
-    graph.addVertex("f")
+    graph.addEdge(0, 1, -1)
+    graph.addEdge(0, 2, 4)
+    graph.addEdge(1, 2, 3)
+    graph.addEdge(1, 3, 2)
+    graph.addEdge(1, 4, 2)
+    graph.addEdge(3, 2, 5)
+    graph.addEdge(3, 1, 1)
+    graph.addEdge(4, 3, -3)
 
-    graph.addEdge("a", "b", 2)
-    graph.addEdge("a", "c", 5)
-    graph.addEdge("b", "c", 6)
-    graph.addEdge("b", "d", 1)
-    graph.addEdge("b", "e", 3)
-    graph.addEdge("c", "f", 8)
-    graph.addEdge("d", "e", 4)
-    graph.addEdge("e", "f", 7)
-
-    graph.dijkstra("a")
-
-    graph.fordbellman("a")
+    graph.fordbellman(0)
 
