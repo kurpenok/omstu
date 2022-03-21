77affd5852749529e5160c09dbd8b9330fadacf0
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-03-21 23:38:00 +0600

    Edited slightly Ford-Bellman algorithm

--- 2 semester/DM/Practics/2022-03-09/FordBellman.py
+++ 2 semester/DM/Practics/2022-03-09/FordBellman.py
@@ -1,11 +1,14 @@
 class Graph:
-    def __init__(self) -> None:
-        graph = []
+    def __init__(self, vertexes: int) -> None:
+        self.vertexes = vertexes
+        self.graph = []
 
+    def connect(self, u: int, v: int, w: int) -> None:
+        self.graph.append([u, v, w])
 
-n = int(input())
-m = int(input())
-v = int(input())
+    def print(self, distances: int) -> None:
+        pass
 
-
+    def fordbellman(self, start: int) -> None:
+        pass
 
