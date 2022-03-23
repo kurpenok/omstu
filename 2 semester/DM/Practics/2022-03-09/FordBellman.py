e21086eb4835f8c288dfdbb1d3eaac6a32ac58ab
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-03-23 14:54:57 +0600

    Added full solution for Ford-Bellman algorithm

--- 2 semester/DM/Practics/2022-03-09/FordBellman.py
+++ 2 semester/DM/Practics/2022-03-09/FordBellman.py
@@ -6,9 +6,31 @@ class Graph:
     def connect(self, u: int, v: int, w: int) -> None:
         self.graph.append([u, v, w])
 
-    def print(self, distances: int) -> None:
-        pass
+    def show(self, distances: list) -> None:
+        print(distances)
 
     def fordbellman(self, start: int) -> None:
-        pass
+        d = [float("inf") for _ in range(self.vertexes)]
+        d[start] = 0
+
+        for _ in range(self.vertexes - 1):
+            for u, v, w in self.graph:
+                if d[v] > d[u] + w:
+                    d[v] = d[u] + w
+
+        self.show(d)
+
+
+graph = Graph(5)
+
+graph.connect(0, 1, -1)
+graph.connect(0, 2, 4)
+graph.connect(1, 2, 3)
+graph.connect(1, 3, 2)
+graph.connect(1, 4, 2)
+graph.connect(3, 2, 5)
+graph.connect(3, 1, 1)
+graph.connect(4, 3, -3)
+
+graph.fordbellman(0)
 
