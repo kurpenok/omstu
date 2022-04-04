7cb3375bbc95b6f82b359fbb25d552b5751338f7
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-04-04 22:11:55 +0600

    Finished third week solutions

--- 2 semester/AP/Practics/2022-03-04/3.8.py
+++ 2 semester/AP/Practics/2022-03-04/3.8.py
@@ -1,4 +1,13 @@
 n = int(input())
 
+mods = []
 
+while n > 1:
+    for i in range(2, n + 1):
+        if not (n % i):
+            mods.append(i)
+            n //= i
+            break
+
+print(*mods)
 
