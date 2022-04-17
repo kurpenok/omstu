5fb2196cf068b0db2ca549fbee77f6d82dd22c84
Kurpenov Kuat <kurpenov2003@protonmail.com>
2022-04-17 12:51:34 +0600

    Added file output to log class

--- 2 semester/AP/Practics/2022-04-12/2.cpp
+++ 2 semester/AP/Practics/2022-04-12/2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <ostream>
+#include <fstream>
+#include <cmath>
 
 class Log {
 private:
@@ -16,18 +18,7 @@ public:
         } else if (number <= 0) {
             throw "[-] Number less than or equal to zero!";
         } else {
-            // The value of the logarithm can only be an integer!
-
-            double v = 1;
-            for (int i = 0; i < number; ++i) {
-                if (v == number) {
-                    value = i;
-                    break;
-                } else if (v > number) {
-                    throw "[-] Could not find degree!";
-                }
-                v *= base;
-            }
+            value = std::log(number) / std::log(base);
         }
     }
     
@@ -80,21 +71,43 @@ Log& Log::operator^(int power) {
     return *this;
 }
 
-// Redefining base change operator doesn't work
-/*
 Log& Log::operator>(double new_base) {
-    this = Log(number, new_base) / Log(base, new_base);
+    value = std::log(number) / std::log(new_base);
     base = new_base;
     return *this;
 }
-*/
+
 
 std::ostream& operator<<(std::ostream& s, Log& l) {
     s << l.get_value();
     return s;
 }
 
+class MyStreambuf:public std::streambuf {
+private:
+    std::streambuf*firstBuf;
+    std::streambuf*secondBuf;
+
+public:
+    MyStreambuf(std::streambuf*_firstBuf,std::streambuf*_secondBuf):firstBuf(_firstBuf),secondBuf(_secondBuf){}
+    std::streamsize xsputn(const char_type *s, std::streamsize n) {
+        firstBuf->sputn(s,n);
+        secondBuf->sputn(s,n);
+        return n;
+    }
+ 
+    int overflow(int_type c) {
+        firstBuf->sputc(c);
+        secondBuf->sputc(c);
+        return c;
+    }
+};
+ 
 int main() {
+    std::ofstream stream("output.txt");
+    MyStreambuf streambuf(stream.rdbuf(),std::cout.rdbuf());
+    std::cout.rdbuf(&streambuf);
+
     Log log(8, 2);
     std::cout << log << std::endl;
 }
