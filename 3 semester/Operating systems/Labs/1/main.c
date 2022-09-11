8b86b8d31459c0122f073cb1d3bf6b36e03d6f0b
Kurpenov Kuat <kurpenov2003@yandex.ru>
2022-09-11 12:22:11 +0600

    Added solution for OS handles task

---
+++ 3 semester/Operating systems/Labs/1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+unsigned int read(int handle, void* buffer, unsigned int len);
+unsigned int write(int handle, void* buffer, unsigned int len);
+
+int main(void) {
+    int cb = 0;
+
+    char buffer[100] = "Buffer";
+    
+    write(0, "[>] Enter: ", 11);
+    cb = read(0, buffer+10, 80);
+    cb += 10;
+
+    write(1, buffer, cb);
+
+
+    printf("sdfsdf");
+    scan(&asdf);
+
+    return 0;
+}
