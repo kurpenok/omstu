bc66ff882bba141fbfe6ed7e45517efa81e1fa35
Kurpenov Kuat <kurpenov2003@yandex.ru>
2022-10-03 22:57:18 +0600

    Reformat OP 1 code

--- 3 semester/Operating systems/Labs/1/main.c
+++ 3 semester/Operating systems/Labs/1/main.c
@@ -6,13 +6,15 @@ unsigned int write(int handle, void* buffer, unsigned int len);
 int main(void) {
     int cb = 0;
 
-    char buffer[100] = "Buffer";
-    
-    write(0, "[>] Enter: ", 11);
-    cb = read(0, buffer+10, 80);
-    cb += 10;
+    char buffer[100] = "Buffer: ";
+
+    write(0, "[>] Enter: ", 90);
+    cb = read(0, buffer+90, 90);
+    cb += 90;
 
     write(1, buffer, cb);
 
     return 0;
 }
+
+/* Write code for windows there */
