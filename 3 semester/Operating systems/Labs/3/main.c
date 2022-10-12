00c8f8e05d77f1bc775f6264d804a712579fb6a3
Kurpenov Kuat <kurpenov2003@yandex.ru>
2022-10-12 22:17:55 +0600

    Added third op task solution

--- 3 semester/Operating systems/Labs/3/main.c
+++ 3 semester/Operating systems/Labs/3/main.c
@@ -1,35 +1,59 @@
+#include <conio.h>
 #include <stdio.h>
-#include <fcntl.h>
+#include <strsafe.h>
+#include <tchar.h>
+#include <windows.h>
 
-unsigned int read(int handle, void* buffer, unsigned int lenght);
-unsigned int write(int handle, const void* buffer, unsigned int lenght);
-void sleep(int time);
-void close(int handle);
+#define BUFFERSIZE 255 
 
-int main() {
-    char buffer[100];
-    int lenght;
+int main(int argc, TCHAR *argv[]) {
+	HANDLE file;
+	Sleep(2000);
+	
+    char buffer[BUFFERSIZE];
 
-    int fhandle;
+	DWORD bytesWasRead = 0;
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	
+    printf("[+] Start:\n");
+	do {
+		file = CreateFile("file",                // name of the write
+		                   GENERIC_READ,          // open for writing
+		                   0,                      // do not share
+		                   NULL,                   // default security
+		                   OPEN_ALWAYS,             // create new file only
+		                   FILE_ATTRIBUTE_NORMAL,  // normal file
+		                   NULL); 
 
-    char fname[] = "test";
+		if (file == INVALID_HANDLE_VALUE && GetLastError() == ERROR_SHARING_VIOLATION) {
+			SetConsoleTextAttribute(hStdOut, FOREGROUND_RED);
+			printf("file error sharing violation \n");
+		}
+		Sleep(1000);
+	} while (file == INVALID_HANDLE_VALUE);
 
-    struct flock lock = {F_WRLCK, SEEK_SET, 0, 0};
+	SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	
+    printf("[+] I begin reading:\n");
 
-    fhandle = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-    fcntl(fhandle, F_SETLKW, &lock);
-
-    write(1, "[>] Enter: ", 20);
-    lenght = read(0, buffer+20, 80);
-    write(fhandle, buffer, lenght+80);
-
-    sleep(10);
-
-    lock.l_type = F_UNLCK;
-
-    fcntl(fhandle, F_SETLK, &lock);
-
-    close(fhandle);
-
+	BOOL flag = ReadFile(file, buffer, BUFFERSIZE, &bytesWasRead, NULL);
+	
+    if (flag == FALSE) {
+		printf("[-] Can`t read %d\n", file);
+		printf("[-] Error is %d\n", GetLastError());
+		CloseHandle(file);
+		getch();
+		return 1;
+	}
+	
+    Sleep(10000);
+	
+    printf("[+] Finished read: %s\n", buffer);
+	
+    CloseHandle(file);
+	
+    getch();
+	
     return 0;
 }
