1b9203211c0bed41136816f30c25f160e21d30ca
Kurpenov Kuat <kurpenov2003@yandex.ru>
2022-10-17 00:00:53 +0600

    Added full solutions fot third task PW

--- 3 semester/Programming workshop/3/main.py
+++ 3 semester/Programming workshop/3/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import scipy
 
 from scipy.linalg import det, lu
-from scipy.stats import norm, uniform
+from scipy.stats import chisquare, norm, uniform
 
 
 def main() -> None:
@@ -21,8 +22,28 @@ def main() -> None:
     sample_norm = norm.rvs(size=100)
     print("[+] Normal sample:", sample_norm)
 
+    print("[+] Mean of NS:", np.mean(sample_norm))
+    print("[+] Mode of NS:", scipy.stats.mode(sample_norm).mode)
+    print("[+] Median of NS:", np.median(sample_norm))
+    print("[+] Minimum of NS:", np.min(sample_norm))
+    print("[+] Maximum of NS:", np.max(sample_norm))
+    print("[+] Standart deviation of NS:", np.std(sample_norm))
+    print("[+] P-value for the null hypothesis: \
+            'The sample distribution is not uniform':",
+          chisquare(sample_norm).pvalue)
+
     sample_uniform = uniform.rvs(size=100)
     print("[+] Uniform sample:", sample_uniform)
+
+    print("[+] Middle value of US:", np.mean(sample_uniform))
+    print("[+] Mode of US:", scipy.stats.mode(sample_uniform).mode)
+    print("[+] Median of US:", np.median(sample_uniform))
+    print("[+] Minimum of US:", np.min(sample_uniform))
+    print("[+] Maximum of US:", np.max(sample_uniform))
+    print("[+] Standart deviation of US:", np.std(sample_uniform))
+    print("[+] P-value for the null hypothesis: \
+            'The sample distribution is not uniform':",
+          chisquare(sample_uniform).pvalue)
 
 
 if __name__ == "__main__":
