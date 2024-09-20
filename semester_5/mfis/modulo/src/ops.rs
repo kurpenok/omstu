ed50b2b5a263a7b41ffd2ca9c996402fd38bc632
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-09-20 23:07:58 +0600

    Supported modulo subtraction

--- semester_5/mfis/modulo/src/ops.rs
+++ semester_5/mfis/modulo/src/ops.rs
@@ -8,6 +8,16 @@ pub fn add(a: i32, b: i32, m: i32) -> i32 {
     sum % m
 }
 
+pub fn sub(a: i32, b: i32, m: i32) -> i32 {
+    let mut sum = a - b;
+
+    while sum < 0 {
+        sum += m;
+    }
+
+    sum % m
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -17,5 +27,12 @@ mod test {
         assert_eq!(add(1, 2, 10), 3);
         assert_eq!(add(6, 8, 12), 2);
         assert_eq!(add(-47, 17, 8), 2);
+    }
+
+    #[test]
+    fn test_modulo_subtraction() {
+        assert_eq!(sub(1, 2, 10), 9);
+        assert_eq!(sub(6, 8, 12), 10);
+        assert_eq!(sub(-47, 17, 8), 0);
     }
 }
