af812224a565ed71fb3d013d1efdf0f5a0aa69cd
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-09-20 23:24:06 +0600

    Supported modulo division

--- semester_5/mfis/modulo/src/ops.rs
+++ semester_5/mfis/modulo/src/ops.rs
@@ -1,3 +1,5 @@
+use crate::invert::invert;
+
 pub fn mod_add(a: i32, b: i32, m: i32) -> i32 {
     let mut sum = a + b;
 
@@ -28,6 +30,21 @@ pub fn mod_mul(a: i32, b: i32, m: i32) -> i32 {
     prod % m
 }
 
+pub fn mod_div(a: i32, b: i32, m: i32) -> Option<i32> {
+    let inverted_b = invert(b, m);
+    if inverted_b.is_none() {
+        return None;
+    }
+
+    let mut quot = a * inverted_b.unwrap();
+
+    while quot < 0 {
+        quot += m;
+    }
+
+    Some(quot % m)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -51,5 +68,12 @@ mod test {
         assert_eq!(mod_mul(1, 2, 10), 2);
         assert_eq!(mod_mul(6, 8, 12), 0);
         assert_eq!(mod_mul(-47, 17, 8), 1);
+    }
+
+    #[test]
+    fn test_modulo_division() {
+        assert_eq!(mod_div(1, 2, 7), Some(4));
+        assert_eq!(mod_div(6, 8, 17), Some(5));
+        assert_eq!(mod_div(-47, 17, 19), Some(14));
     }
 }
