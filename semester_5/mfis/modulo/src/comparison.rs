be818575100a49318ec4899ba6e504a121cec794
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-04 14:47:06 +0600

    Fixed modulo comparison system solver

--- semester_5/mfis/modulo/src/comparison.rs
+++ semester_5/mfis/modulo/src/comparison.rs
@@ -1,7 +1,40 @@
+use std::collections::HashSet;
+
 use crate::{
     gcd::gcd,
     ops::{mod_add, mod_div},
 };
+
+fn is_solution(x: i32, y: i32, a: i32, b: i32, c: i32, d: i32, m: i32) -> bool {
+    let eq_1: i32 = a * x + y;
+    let eq_2: i32 = c * x + y;
+
+    (eq_1 % m == b && eq_2 % m == d) || (eq_1 % m == d && eq_2 % m == b)
+}
+
+fn get_solutions(x: i32, a: i32, b: i32, c: i32, d: i32, m: i32) -> HashSet<Vec<i32>> {
+    let mut solutions: HashSet<Vec<i32>> = HashSet::new();
+
+    let y_1 = mod_add(0, b - a * x, m);
+    let y_2 = mod_add(0, b - c * x, m);
+    let y_3 = mod_add(0, d - a * x, m);
+    let y_4 = mod_add(0, d - c * x, m);
+
+    if is_solution(x, y_1, a, b, c, d, m) {
+        solutions.insert(vec![x, y_1]);
+    }
+    if is_solution(x, y_2, a, b, c, d, m) {
+        solutions.insert(vec![x, y_2]);
+    }
+    if is_solution(x, y_3, a, b, c, d, m) {
+        solutions.insert(vec![x, y_3]);
+    }
+    if is_solution(x, y_4, a, b, c, d, m) {
+        solutions.insert(vec![x, y_4]);
+    }
+
+    solutions
+}
 
 fn solve_comparison(a: i32, b: i32, m: i32) -> Option<Vec<Vec<i32>>> {
     let divisor = gcd(a, m);
@@ -37,10 +70,8 @@ fn solve_comparison(a: i32, b: i32, m: i32) -> Option<Vec<Vec<i32>>> {
 }
 
 fn solve_comparison_system(a: i32, b: i32, c: i32, d: i32, m: i32) -> Option<Vec<Vec<i32>>> {
-    let mut solutions = Vec::new();
+    let mut solutions: HashSet<Vec<i32>> = HashSet::new();
 
-    // ATTENTION!
-    // Coefficients of first function must be GREATER than second function!
     let solutions_1 = solve_comparison((a - c).abs(), mod_add(0, b - d, m), m);
     let solutions_2 = solve_comparison((a - c).abs(), mod_add(0, d - b, m), m);
 
@@ -50,15 +81,18 @@ fn solve_comparison_system(a: i32, b: i32, c: i32, d: i32, m: i32) -> Option<Vec
 
     if solutions_1.is_some() {
         for solution in &solutions_1.unwrap() {
-            solutions.push(vec![solution[0], mod_add(0, b - a * solution[0], m)]);
+            solutions.extend(get_solutions(solution[0], a, b, c, d, m));
         }
     }
 
     if solutions_2.is_some() {
         for solution in &solutions_2.unwrap() {
-            solutions.push(vec![solution[0], mod_add(0, d - a * solution[0], m)]);
+            solutions.extend(get_solutions(solution[0], a, b, c, d, m));
         }
     }
+
+    let mut solutions: Vec<Vec<i32>> = Vec::from_iter(solutions);
+    solutions.sort();
 
     Some(solutions)
 }
@@ -100,7 +134,7 @@ mod test {
 
         assert_eq!(
             solve(18, 9, Some(5), Some(7), 15),
-            Some(vec![vec![14, 12], vec![1, 4]])
+            Some(vec![vec![1, 4], vec![14, 12]])
         );
         assert_eq!(
             solve(14, 8, Some(5), Some(19), 32),
