cbb3f8551b594dbd4f99b1cc741ec4c66b47bb4d
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-04 22:47:59 +0600

    Formatted affine cipher analysis

--- semester_5/mfis/affine_cipher/src/analysis.rs
+++ semester_5/mfis/affine_cipher/src/analysis.rs
@@ -14,7 +14,7 @@ pub fn get_frequency(s: &String) -> HashMap<char, usize> {
 }
 
 pub fn get_most_common_chars(frequency: &HashMap<char, usize>, n: usize) -> Vec<char> {
-    let mut values: Vec<(char, usize)> = frequency.iter().map(|(k, v)| (*k, *v)).collect();
+    let mut values: Vec<(char, usize)> = frequency.iter().map(|(&k, &v)| (k, v)).collect();
     values.sort_by(|a, b| b.1.cmp(&a.1));
     values.into_iter().take(n).map(|(k, _)| k).collect()
 }
