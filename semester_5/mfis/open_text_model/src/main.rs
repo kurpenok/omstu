11625ba92fa91504e5db59c8b0d6d6606fd168d8
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-05 13:28:11 +0600

    Added k-gram entropy cli for open text model

--- semester_5/mfis/open_text_model/src/main.rs
+++ semester_5/mfis/open_text_model/src/main.rs
@@ -1,3 +1,18 @@
+mod cli_actions;
+mod cli_args;
 mod k_gram;
 
-fn main() {}
+use clap::Parser;
+use cli_actions::cli_entropy;
+use cli_args::{Cli, Commands};
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Entropy(data)) => cli_entropy(data),
+        None => {
+            println!("[-] Error reading parameters!");
+        }
+    }
+}
