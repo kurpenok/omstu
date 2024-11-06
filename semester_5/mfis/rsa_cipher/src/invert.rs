2b78cf2854cd1c9edb9e46cd6c3828d2c9521cc6
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-07 01:12:13 +0600

    Formatted bigint invert by modulo

--- semester_5/mfis/rsa_cipher/src/invert.rs
+++ semester_5/mfis/rsa_cipher/src/invert.rs
@@ -19,28 +19,26 @@ pub fn invert(a: &BigInt, m: &BigInt) -> Option<BigInt> {
 
 #[cfg(test)]
 mod test {
-    use num_bigint::ToBigInt;
-
     use super::*;
 
     #[test]
     fn test_bigint_invert() {
-        assert_eq!(
-            invert(&2.to_bigint().unwrap(), &6.to_bigint().unwrap()),
-            None
-        );
-        assert_eq!(
-            invert(&8.to_bigint().unwrap(), &64.to_bigint().unwrap()),
-            None
-        );
+        let a = BigInt::from(2);
+        let b = BigInt::from(6);
+        assert_eq!(invert(&a, &b), None);
 
-        assert_eq!(
-            invert(&3.to_bigint().unwrap(), &7.to_bigint().unwrap()),
-            Some(5.to_bigint().unwrap())
-        );
-        assert_eq!(
-            invert(&3.to_bigint().unwrap(), &26.to_bigint().unwrap()),
-            Some(9.to_bigint().unwrap())
-        );
+        let a = BigInt::from(8);
+        let b = BigInt::from(64);
+        assert_eq!(invert(&a, &b), None);
+
+        let a = BigInt::from(3);
+        let b = BigInt::from(7);
+        let c = BigInt::from(5);
+        assert_eq!(invert(&a, &b), Some(c));
+
+        let a = BigInt::from(3);
+        let b = BigInt::from(26);
+        let c = BigInt::from(9);
+        assert_eq!(invert(&a, &b), Some(c));
     }
 }
