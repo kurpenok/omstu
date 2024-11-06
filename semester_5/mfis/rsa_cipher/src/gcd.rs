542817c21bd7a18312fed7200426534e82fe1e25
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-06 20:08:18 +0600

    Added bigint extended gcd for rsa

--- semester_5/mfis/rsa_cipher/src/gcd.rs
+++ semester_5/mfis/rsa_cipher/src/gcd.rs
@@ -1,5 +1,10 @@
 use num_bigint::BigInt;
-use num_traits::Zero;
+use num_traits::{One, Zero};
+
+pub struct BezuCoefs {
+    pub alpha: BigInt,
+    pub beta: BigInt,
+}
 
 pub fn gcd(a: &BigInt, b: &BigInt) -> BigInt {
     if b.is_zero() {
@@ -7,6 +12,25 @@ pub fn gcd(a: &BigInt, b: &BigInt) -> BigInt {
     }
 
     gcd(b, &(a % b))
+}
+
+pub fn extended_gcd(a: &BigInt, b: &BigInt, coefs: &mut BezuCoefs) -> BigInt {
+    if a.is_zero() {
+        coefs.alpha = BigInt::zero();
+        coefs.beta = BigInt::one();
+        return b.clone();
+    }
+
+    let mut temp_coefs = BezuCoefs {
+        alpha: BigInt::zero(),
+        beta: BigInt::zero(),
+    };
+    let divider = extended_gcd(&(b % a), a, &mut temp_coefs);
+
+    coefs.alpha = temp_coefs.beta - (b / a) * &temp_coefs.alpha;
+    coefs.beta = temp_coefs.alpha;
+
+    divider
 }
 
 #[cfg(test)]
@@ -36,5 +60,20 @@ mod test {
             gcd(&20.to_bigint().unwrap(), &20.to_bigint().unwrap()),
             20.to_bigint().unwrap()
         );
+    }
+
+    #[test]
+    fn test_bigint_extended_gcd() {
+        let mut coefs = BezuCoefs {
+            alpha: 0.to_bigint().unwrap(),
+            beta: 0.to_bigint().unwrap(),
+        };
+
+        assert_eq!(
+            extended_gcd(&3.to_bigint().unwrap(), &7.to_bigint().unwrap(), &mut coefs),
+            1.to_bigint().unwrap()
+        );
+        assert_eq!(coefs.alpha, -2.to_bigint().unwrap());
+        assert_eq!(coefs.beta, 1.to_bigint().unwrap());
     }
 }
