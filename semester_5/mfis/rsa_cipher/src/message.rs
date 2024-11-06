15437af3d6d1bb9d7523c79e5a06e092570cb7be
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-06 22:32:52 +0600

    Replaced usize usings to BigInt

--- semester_5/mfis/rsa_cipher/src/message.rs
+++ semester_5/mfis/rsa_cipher/src/message.rs
@@ -1,20 +1,38 @@
 use std::collections::HashMap;
 
-pub fn encode(abc: &HashMap<char, usize>, message: &String) -> Vec<usize> {
+use num_bigint::BigInt;
+
+pub fn encode(abc: &HashMap<char, BigInt>, message: &String) -> Vec<BigInt> {
     message
         .chars()
-        .filter_map(|c| abc.get(&c).copied())
+        .filter_map(|c| abc.get(&c).cloned())
         .collect()
 }
 
 #[cfg(test)]
 mod test {
+    use num_bigint::ToBigInt;
+
     use super::*;
 
     #[test]
     fn test_encode_message() {
-        let abc = HashMap::from([('a', 10), ('b', 11), (' ', 99)]);
+        let abc = HashMap::from([
+            ('a', 10.to_bigint().unwrap()),
+            ('b', 11.to_bigint().unwrap()),
+            (' ', 99.to_bigint().unwrap()),
+        ]);
         let message = "ab ba".to_string();
-        assert_eq!(encode(&abc, &message), vec![10, 11, 99, 11, 10]);
+
+        assert_eq!(
+            encode(&abc, &message),
+            vec![
+                10.to_bigint().unwrap(),
+                11.to_bigint().unwrap(),
+                99.to_bigint().unwrap(),
+                11.to_bigint().unwrap(),
+                10.to_bigint().unwrap()
+            ]
+        );
     }
 }
