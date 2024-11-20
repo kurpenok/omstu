865de49b2d823896764786ec3fbecc06df5c386c
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-20 22:42:18 +0600

    [S5 MFIS] Modified division by modulo operation

--- semester_5/mfis/modulo/src/modules/ops.rs
+++ semester_5/mfis/modulo/src/modules/ops.rs
@@ -1,21 +1,50 @@
-pub fn mod_add(a: i32, b: i32, m: i32) -> i32 {
+use super::invert::invert;
+
+pub fn mod_add(a: i32, b: i32, m: usize) -> usize {
     let mut sum = a + b;
 
     while sum < 0 {
-        sum += m;
+        sum += m as i32;
     }
 
-    sum % m
+    sum as usize % m
 }
 
-pub fn mod_sub(a: i32, b: i32, m: i32) -> i32 {
+pub fn mod_sub(a: i32, b: i32, m: usize) -> usize {
     let mut diff = a - b;
 
     while diff < 0 {
-        diff += m;
+        diff += m as i32;
     }
 
-    diff % m
+    diff as usize % m
+}
+
+pub fn mod_mul(a: i32, b: i32, m: usize) -> usize {
+    let mut prod = a * b;
+
+    while prod < 0 {
+        prod += m as i32;
+    }
+
+    prod as usize % m
+}
+
+pub fn mod_div(a: usize, b: usize, m: usize) -> Option<usize> {
+    match invert(b, m) {
+        Some(inverted_b) => Some(a * inverted_b % m),
+        None => None,
+    }
+}
+
+pub fn mod_pow(a: i32, p: u32, m: usize) -> usize {
+    let mut prod = a.pow(p);
+
+    while prod < 0 {
+        prod += m as i32;
+    }
+
+    prod as usize % m
 }
 
 #[cfg(test)]
@@ -34,5 +63,26 @@ mod test {
         assert_eq!(mod_sub(1, 2, 10), 9);
         assert_eq!(mod_sub(6, 8, 12), 10);
         assert_eq!(mod_sub(-47, 17, 8), 0);
+    }
+
+    #[test]
+    fn test_modulo_multiplication() {
+        assert_eq!(mod_mul(1, 2, 10), 2);
+        assert_eq!(mod_mul(6, 8, 12), 0);
+        assert_eq!(mod_mul(-47, 17, 8), 1);
+    }
+
+    #[test]
+    fn test_modulo_division() {
+        assert_eq!(mod_div(1, 2, 7), Some(4));
+        assert_eq!(mod_div(6, 8, 17), Some(5));
+        assert_eq!(mod_div(10, 17, 19), Some(14));
+    }
+
+    #[test]
+    fn test_modulo_exponentiation() {
+        assert_eq!(mod_pow(1, 2, 10), 1);
+        assert_eq!(mod_pow(6, 8, 12), 0);
+        assert_eq!(mod_pow(-7, 7, 8), 1);
     }
 }
