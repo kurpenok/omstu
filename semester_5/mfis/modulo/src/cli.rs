8cd818386368b5d53912c25e9b1bb30ef485ae32
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-20 21:53:04 +0600

    [S5 MFIS] Added read from console function

---
+++ semester_5/mfis/modulo/src/cli.rs
@@ -0,0 +1,14 @@
+use std::io::Write;
+
+pub fn console_read(prompt: &str) -> String {
+    let mut value = String::new();
+
+    print!("{}", prompt);
+    let _ = std::io::stdout().flush();
+    std::io::stdin()
+        .read_line(&mut value)
+        .ok()
+        .expect("[-] Failed to read value!");
+
+    value.trim().to_lowercase().to_string()
+}
