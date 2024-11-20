e970d9f29afbb9004f4f1b7efef136301057de6c
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-20 21:52:25 +0600

    [S5 MFIS] Removed unnecessary modulo ops

--- semester_5/mfis/modulo/src/ops.rs
+++ semester_5/mfis/modulo/src/ops.rs
@@ -1,5 +1,3 @@
-use crate::invert::invert;
-
 pub fn mod_add(a: i32, b: i32, m: i32) -> i32 {
     let mut sum = a + b;
 
@@ -20,41 +18,6 @@ pub fn mod_sub(a: i32, b: i32, m: i32) -> i32 {
     diff % m
 }
 
-pub fn mod_mul(a: i32, b: i32, m: i32) -> i32 {
-    let mut prod = a * b;
-
-    while prod < 0 {
-        prod += m;
-    }
-
-    prod % m
-}
-
-pub fn mod_div(a: i32, b: i32, m: i32) -> Option<i32> {
-    let inverted_b = invert(b, m);
-    if inverted_b.is_none() {
-        return None;
-    }
-
-    let mut quot = a * inverted_b.unwrap();
-
-    while quot < 0 {
-        quot += m;
-    }
-
-    Some(quot % m)
-}
-
-pub fn mod_pow(a: i32, p: u32, m: i32) -> i32 {
-    let mut prod = a.pow(p);
-
-    while prod < 0 {
-        prod += m;
-    }
-
-    prod % m
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -71,26 +34,5 @@ mod test {
         assert_eq!(mod_sub(1, 2, 10), 9);
         assert_eq!(mod_sub(6, 8, 12), 10);
         assert_eq!(mod_sub(-47, 17, 8), 0);
-    }
-
-    #[test]
-    fn test_modulo_multiplication() {
-        assert_eq!(mod_mul(1, 2, 10), 2);
-        assert_eq!(mod_mul(6, 8, 12), 0);
-        assert_eq!(mod_mul(-47, 17, 8), 1);
-    }
-
-    #[test]
-    fn test_modulo_division() {
-        assert_eq!(mod_div(1, 2, 7), Some(4));
-        assert_eq!(mod_div(6, 8, 17), Some(5));
-        assert_eq!(mod_div(-47, 17, 19), Some(14));
-    }
-
-    #[test]
-    fn test_modulo_exponentiation() {
-        assert_eq!(mod_pow(1, 2, 10), 1);
-        assert_eq!(mod_pow(6, 8, 12), 0);
-        assert_eq!(mod_pow(-7, 7, 8), 1);
     }
 }
