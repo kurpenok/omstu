e1d470d5304648f27623e7a19e9eb442e5cf8fbb
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-21 13:28:21 +0600

    [S5 MFIS] Added solve comparisons system cli

--- semester_5/mfis/modulo/src/cli/cli_solve.rs
+++ semester_5/mfis/modulo/src/cli/cli_solve.rs
@@ -1,4 +1,7 @@
-use crate::{modules::solve::solve_comparison, SEPARATOR};
+use crate::{
+    modules::solve::{solve_comparison, solve_comparisons_system},
+    SEPARATOR,
+};
 
 use super::console_read;
 
@@ -18,7 +21,21 @@ pub fn cli_solve(system: bool) {
             None => println!("[-] Comparison has no solutions!"),
         }
     } else {
-        todo!()
+        let a = console_read("[>] Enter number a: ").parse::<usize>();
+        let b = console_read("[>] Enter number b: ").parse::<usize>();
+        let c = console_read("[>] Enter number c: ").parse::<usize>();
+        let d = console_read("[>] Enter number d: ").parse::<usize>();
+        let m = console_read("[>] Enter modulo: ").parse::<usize>();
+
+        if a.is_err() || b.is_err() || c.is_err() || d.is_err() || m.is_err() {
+            println!("[-] Incorrect values!");
+            return;
+        }
+
+        match solve_comparisons_system(a.unwrap(), b.unwrap(), c.unwrap(), d.unwrap(), m.unwrap()) {
+            Some(solutions) => println!("[+] Solutions: {:?}", solutions),
+            None => println!("[-] Comparison has no solutions!"),
+        }
     }
     println!("{}", SEPARATOR);
 }
--- semester_5/mfis/modulo/src/modules/solve.rs
+++ semester_5/mfis/modulo/src/modules/solve.rs
@@ -1,4 +1,7 @@
-use super::{gcd::gcd, ops::mod_div};
+use super::{
+    gcd::gcd,
+    ops::{mod_add, mod_div},
+};
 
 pub fn solve_comparison(a: usize, b: usize, m: usize) -> Option<Vec<usize>> {
     let divisor = gcd(a, m);
@@ -33,6 +36,42 @@ pub fn solve_comparison(a: usize, b: usize, m: usize) -> Option<Vec<usize>> {
     Some(solutions)
 }
 
+pub fn solve_comparisons_system(
+    a: usize,
+    b: usize,
+    c: usize,
+    d: usize,
+    m: usize,
+) -> Option<Vec<Vec<usize>>> {
+    let mut solutions: Vec<Vec<usize>> = Vec::new();
+
+    let mut solutions_x: Vec<usize> = Vec::new();
+    let (new_a, new_b) = if a < c {
+        (c - a, mod_add(0, d as i32 - b as i32, m))
+    } else {
+        (a - c, mod_add(0, b as i32 - d as i32, m))
+    };
+
+    match solve_comparison(new_a, new_b, m) {
+        Some(solutions) => {
+            for s in solutions {
+                solutions_x.push(s);
+            }
+        }
+        None => (),
+    }
+
+    for x in solutions_x {
+        let y = mod_add(0, b as i32 - a as i32 * x as i32, m);
+        solutions.push(vec![x, y]);
+    }
+
+    if solutions.len() == 0 {
+        return None;
+    }
+    Some(solutions)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -44,5 +83,20 @@ mod test {
 
         assert_eq!(solve_comparison(1, 2, 3), Some(vec![2]));
         assert_eq!(solve_comparison(6, 8, 10), Some(vec![3, 8]));
+    }
+
+    #[test]
+    fn test_comparison_system_solve() {
+        assert_eq!(solve_comparisons_system(0, 9, 8, 15, 32), None);
+        assert_eq!(solve_comparisons_system(10, 9, 6, 3, 8), None);
+
+        assert_eq!(
+            solve_comparisons_system(18, 9, 5, 7, 15),
+            Some(vec![vec![14, 12]]),
+        );
+        assert_eq!(
+            solve_comparisons_system(14, 8, 5, 19, 32),
+            Some(vec![vec![13, 18]]),
+        );
     }
 }
