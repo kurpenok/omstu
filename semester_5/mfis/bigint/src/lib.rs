922e93961a2442073a011c2f75ad3d70fa1b7174
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-12-02 16:40:28 +0600

    [S5 MFIS] Added library for working with bigint

---
+++ semester_5/mfis/bigint/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod gcd;
+pub mod inverse;
+pub mod prime;
---
+++ semester_5/mfis/bigint/src/prime.rs
@@ -0,0 +1,47 @@
+use num_bigint::{BigInt, RandBigInt};
+use num_traits::{One, Zero};
+use rand::thread_rng;
+
+pub fn is_prime(n: &BigInt) -> bool {
+    if n <= &BigInt::one() {
+        return false;
+    } else if n == &BigInt::from(2) {
+        return true;
+    } else if (n % BigInt::from(2)).is_zero() {
+        return false;
+    }
+
+    let mut i = BigInt::from(3);
+    while &i <= &n.sqrt() {
+        if n % &i == BigInt::zero() {
+            return false;
+        }
+        i += BigInt::from(2);
+    }
+
+    true
+}
+
+pub fn generate_prime(bits: u64) -> BigInt {
+    let mut rng = thread_rng();
+    loop {
+        let number = rng.gen_bigint(bits);
+        if is_prime(&number) {
+            break number;
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_bigint_is_prime() {
+        assert_eq!(is_prime(&BigInt::from(0)), false);
+        assert_eq!(is_prime(&BigInt::from(1)), false);
+        assert_eq!(is_prime(&BigInt::from(2)), true);
+        assert_eq!(is_prime(&BigInt::from(3)), true);
+        assert_eq!(is_prime(&BigInt::from(4)), false);
+    }
+}
