271fdd2c38b264b15eb06f7cd33d58d341968824
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-12-03 20:55:20 +0600

    [S5 MFIS] Unlock cli in find discrete logarithm algorithm

--- semester_5/mfis/t7_discrete_logarithm/src/cli/cli_logarithm.rs
+++ semester_5/mfis/t7_discrete_logarithm/src/cli/cli_logarithm.rs
@@ -1,19 +1,17 @@
 use crate::{cli::console_read, modules::logarithm::find_logarithm, SEPARATOR};
 
 pub fn cli_logarithm() {
-    //let a = console_read("[>] Enter number a: ").parse::<usize>();
-    //let b = console_read("[>] Enter number b: ").parse::<usize>();
-    //let p = console_read("[>] Enter number p: ").parse::<usize>();
-    //
-    //if a.is_err() || b.is_err() || p.is_err() {
-    //    println!("[-] Incorrect values!");
-    //    println!("{}", SEPARATOR);
-    //    return;
-    //}
+    let a = console_read("[>] Enter number a: ").parse::<usize>();
+    let b = console_read("[>] Enter number b: ").parse::<usize>();
+    let p = console_read("[>] Enter number p: ").parse::<usize>();
 
-    //let discrete_logarithm = find_logarithm(a.unwrap(), b.unwrap(), p.unwrap());
-    //println!("[+] Discrete logarithm: {}", discrete_logarithm);
+    if a.is_err() || b.is_err() || p.is_err() {
+        println!("[-] Incorrect values!");
+        println!("{}", SEPARATOR);
+        return;
+    }
 
-    println!("[+] Discrete logarithm: {}", find_logarithm(6, 15, 109));
+    let discrete_logarithm = find_logarithm(a.unwrap(), b.unwrap(), p.unwrap());
+    println!("[+] Discrete logarithm: {}", discrete_logarithm);
     println!("{}", SEPARATOR);
 }
