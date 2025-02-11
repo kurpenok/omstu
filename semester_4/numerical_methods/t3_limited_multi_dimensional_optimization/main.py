b225841fdac99ae94b240db0ad9018fd047e23dd
Kuat Kurpenov <kurpenov2003@yandex.ru>
2025-02-11 12:01:41 +0600

    [S4 NM] Removed solutions folder

--- semester_4/numerical_methods/t3_limited_multi_dimensional_optimization/main.py
+++
@@ -1,9 +0,0 @@
-from lagrange_multipliers import langrange_multipliers_search
-
-
-def main() -> None:
-    print(f"[+] Lagrange: {langrange_multipliers_search()}")
-
-
-if __name__ == "__main__":
-    main()
