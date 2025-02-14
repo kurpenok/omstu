7ceed49b7e98c47a88d43ca1c3b45c0ce15aea32
Kuat Kurpenov <kurpenov2003@yandex.ru>
2025-02-14 23:32:02 +0600

    [S4 OPT] Implemented steepest descent method for laboratory work of multidimensional optimization

--- semester_4/optimization/lw2_multidimensional_optimization/src/main.rs
+++ semester_4/optimization/lw2_multidimensional_optimization/src/main.rs
@@ -1,9 +1,12 @@
 mod gauss_seidel;
 mod golden_ratio;
+mod gradient;
 mod powell;
+mod steepest_descent;
 
 use gauss_seidel::gauss_seidel_search;
 use powell::powell;
+use steepest_descent::steepest_descent;
 
 fn f(x: &Vec<f64>) -> f64 {
     let a: f64 = 3.0;
@@ -18,10 +21,14 @@ fn f(x: &Vec<f64>) -> f64 {
 fn main() {
     let eps: f64 = 0.0001;
     let max_iter: usize = 1000;
+    let grad_h: f64 = 0.00000001;
 
     let (x, y) = gauss_seidel_search(f, vec![0.0, 0.0], eps, max_iter);
     println!("[+] Gauss-Seidel search: ({:.2?}, {:.2})", x, y);
 
     let (x, y) = powell(f, vec![0.0, 0.0], eps, max_iter);
     println!("[+] Powell search: ({:.2?}, {:.2})", x, y);
+
+    let (x, y) = steepest_descent(f, vec![0.0, 0.0], eps, max_iter, grad_h);
+    println!("[+] Steepest descent search: ({:.2?}, {:.2})", x, y);
 }
